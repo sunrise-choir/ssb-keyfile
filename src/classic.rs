aa21ed16f7693df68452328404b2c8693f8ce879
sean billig <sean.billig@gmail.com>
2020-12-17 20:17:51 -0800

    Clean up API, add generate and generate_at_path convenience fns

--- src/classic.rs
+++ src/classic.rs
@@ -1,20 +1,112 @@
 use serde::{Deserialize, Serialize};
 use serde_json::error::Error as JsonError;
-pub use ssb_crypto::{AsBytes, Keypair};
+use ssb_crypto::AsBytes;
+pub use ssb_crypto::Keypair;
 use std::fs::File;
-use std::io::{self, Read};
+use std::io::{self, Read, Write};
 use std::path::Path;
 use thiserror::Error;
 
-/// Read the key file at the given path and parse the keys from it.
-pub fn load_keys_from_path<P: AsRef<Path>>(path: P) -> Result<Keypair, KeyFileError> {
-    let mut f = File::open(&path)?;
-
+/// Read and parse the key file from the given [Read] stream.
+/// May read more bytes than absolutely necessary.
+pub fn read<R: Read>(mut r: R) -> Result<Keypair, KeyFileError> {
     // A standard js ssb secret file is 727 bytes
     let mut buf = [0u8; 1024];
-    f.read(&mut buf)?;
+    r.read(&mut buf)?;
     let sec_str = std::str::from_utf8(&buf)?;
-    keys_from_str(sec_str)
+    read_from_str(sec_str)
+}
+
+/// Read and parse the key file at the given path.
+pub fn read_from_path<P: AsRef<Path>>(path: P) -> Result<Keypair, KeyFileError> {
+    let f = File::open(&path)?;
+    read(f)
+}
+
+/// Load keys from the string contents of a js ssb secret file.
+pub fn read_from_str(s: &str) -> Result<Keypair, KeyFileError> {
+    let raw_sec_str = s
+        .lines()
+        .filter(|s| !s.starts_with('#'))
+        .collect::<Vec<&str>>()
+        .concat();
+    let sec_str = raw_sec_str.trim_matches(char::from(0));
+    let sec = serde_json::from_str::<SecretFile>(&sec_str)?;
+    if let Some(kp) = Keypair::from_base64(&sec.private) {
+        Ok(kp)
+    } else {
+        Err(KeyFileError::Decode)
+    }
+}
+
+/// Create a string containing the content of a new secret file for the given keys,
+/// in js ssb commented-json format.
+pub fn write(keypair: &Keypair, mut w: impl Write) -> Result<(), io::Error> {
+    let mut id = encode_key(&keypair.public.0);
+    id.insert(0, '@');
+
+    let kf = SecretFileFull {
+        curve: "ed25519",
+        public: encode_key(&keypair.public.0),
+        private: encode_key(keypair.as_bytes()),
+        id: id,
+    };
+
+    w.write(PRE_COMMENT.as_bytes())?;
+    let formatter = serde_json::ser::PrettyFormatter::with_indent(b"");
+    let mut ser = serde_json::Serializer::with_formatter(&mut w, formatter);
+    kf.serialize(&mut ser).unwrap();
+
+    w.write(POST_COMMENT.as_bytes())?;
+    w.write(kf.id.as_bytes())?;
+    Ok(())
+}
+
+/// Write the given [Keypair] to a new file at the specified path. `path` should include the file name.
+/// Fails if the path exists, or if the path is a directory.
+pub fn write_to_path<P: AsRef<Path>>(keypair: &Keypair, path: P) -> Result<(), io::Error> {
+    if path.as_ref().is_dir() {
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            "expected a path to a file",
+        ));
+    }
+
+    if path.as_ref().exists() {
+        return Err(io::Error::new(io::ErrorKind::AlreadyExists, "file exists"));
+    }
+
+    if let Some(dir) = path.as_ref().parent() {
+        if !dir.exists() {
+            std::fs::create_dir_all(&dir)?
+        }
+    }
+
+    let f = File::create(&path)?;
+    write(keypair, f)
+}
+
+/// Create a string with the js ssb commented-json format string encoding of the given [Keypair].
+pub fn write_to_string(keypair: &Keypair) -> String {
+    let mut out = Vec::with_capacity(727);
+    write(keypair, io::Cursor::new(&mut out)).unwrap();
+    String::from_utf8(out).unwrap()
+}
+
+/// Generate a new [Keypair] and write it to the given [Write] stream.
+pub fn generate(w: impl Write) -> Result<Keypair, io::Error> {
+    let keypair = Keypair::generate();
+    write(&keypair, w)?;
+    Ok(keypair)
+}
+
+/// Generate a new [Keypair] and write it to a new file at the specified [Path].
+/// The path should include the file name.
+/// Fails if the path exists, or if the path is a directory.
+pub fn generate_at_path<P: AsRef<Path>>(path: P) -> Result<Keypair, io::Error> {
+    let keypair = Keypair::generate();
+    write_to_path(&keypair, path)?;
+    Ok(keypair)
 }
 
 // The libsodium "secret" key also contains the public key,
@@ -32,53 +124,10 @@ struct SecretFileFull {
     id: String,
 }
 
-/// Load keys from the string contents of a js ssb secret file.
-pub fn keys_from_str(s: &str) -> Result<Keypair, KeyFileError> {
-    let raw_sec_str = s
-        .lines()
-        .filter(|s| !s.starts_with('#'))
-        .collect::<Vec<&str>>()
-        .concat();
-    let sec_str = raw_sec_str.trim_matches(char::from(0));
-    let sec = serde_json::from_str::<SecretFile>(&sec_str)?;
-    if let Some(kp) = Keypair::from_base64(&sec.private) {
-        Ok(kp)
-    } else {
-        Err(KeyFileError::Decode)
-    }
-}
-
 fn encode_key(bytes: &[u8]) -> String {
     let mut out = base64::encode_config(bytes, base64::STANDARD);
     out.push_str(".ed25519");
     out
-}
-
-/// Create a string containing the content of a new secret file for the given keys,
-/// in js ssb commented-json format.
-pub fn new_keyfile_string(keypair: &Keypair) -> String {
-    let mut msg = Vec::with_capacity(727);
-
-    let mut id = encode_key(&keypair.public.0);
-    id.insert(0, '@');
-
-    let kf = SecretFileFull {
-        curve: "ed25519",
-        public: encode_key(&keypair.public.0),
-        private: encode_key(keypair.as_bytes()),
-        id: id,
-    };
-
-    msg.extend_from_slice(PRE_COMMENT.as_bytes());
-
-    let formatter = serde_json::ser::PrettyFormatter::with_indent(b"");
-    let mut ser = serde_json::Serializer::with_formatter(&mut msg, formatter);
-    kf.serialize(&mut ser).unwrap();
-
-    msg.extend_from_slice(POST_COMMENT.as_bytes());
-    msg.extend_from_slice(kf.id.as_bytes());
-
-    String::from_utf8(msg).unwrap()
 }
 
 /// The reasons why reading keys from a file can fail.
@@ -127,7 +176,7 @@ mod tests {
 
     #[test]
     fn read_js_file() {
-        let keypair = load_keys_from_path(test_data_file("secret")).unwrap();
+        let keypair = read_from_path(test_data_file("secret")).unwrap();
 
         assert_eq!(
             keypair.public.0,
@@ -137,7 +186,7 @@ mod tests {
             ]
         );
 
-        let newkf = new_keyfile_string(&keypair);
+        let newkf = write_to_string(&keypair);
         assert_eq!(
             newkf,
             std::fs::read_to_string(test_data_file("secret")).unwrap()
@@ -146,7 +195,7 @@ mod tests {
 
     #[test]
     fn read_go_file() {
-        let keypair = load_keys_from_path(test_data_file("secret")).unwrap();
+        let keypair = read_from_path(test_data_file("secret")).unwrap();
         assert_eq!(
             keypair.public.0,
             [
@@ -154,5 +203,27 @@ mod tests {
                 250, 80, 30, 63, 64, 189, 150, 175, 72, 86, 84, 12, 162, 215
             ]
         );
+    }
+
+    #[test]
+    fn generate_file() {
+        let dir = tempfile::TempDir::new().unwrap();
+
+        // path must not be a dir
+        assert!(generate_at_path(dir.path()).is_err());
+        let path = dir.path().join("secret");
+        let kp = generate_at_path(&path).unwrap();
+
+        // file must not exist
+        assert!(generate_at_path(&path).is_err());
+
+        let kp2 = read_from_path(&path).unwrap();
+        assert_eq!(kp.public, kp2.public);
+
+        // should create intermediate dirs if they don't exist
+        let path = dir.path().join("foo").join("bar");
+        let kp = generate_at_path(&path).unwrap();
+        let kp2 = read_from_path(&path).unwrap();
+        assert_eq!(kp.public, kp2.public);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 //! Read ssb keyfiles, as created by the js implementation.
+//!
+//! Re-exports [Keypair] from the [ssb-crypto](https://docs.rs/ssb-crypto) crate.
 
 mod classic;
 pub use classic::*;
